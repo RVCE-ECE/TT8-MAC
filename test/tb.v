9ecc18af5802c2bff56a0db31a6b5b78e9492a65
RVCE-ECE <shylashreen@rvce.edu.in>
2024-09-02 21:41:53 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -17,8 +17,10 @@ module tb ();
   reg clk;
   reg rst_n;
   reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
+  wire [7:0] ui_in;
+  wire [7:0] uio_in;
+  reg [7:0] ui_in_reg;
+  reg [7:0] uio_in_reg;
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
@@ -41,38 +43,46 @@ module tb ();
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
   );
-initial begin
-        clk = 0;
-        forever #5 clk = ~clk;  
-    end
-    initial begin
-        rst_n = 0;  
-        ui_in = 8'b00000000;
-        uio_in = 8'b00000000;
-        #10;
-        rst_n = 1;  
-        ui_in = 8'b00000011; // 3
-        uio_in = 8'b00000010; // 2
-        #20; 
-        ui_in = 8'b00000001; // 1
-        uio_in = 8'b00000100; // 4
-        #20; 
-        ui_in = 8'b00000101; // 5
-        uio_in = 8'b00000011; // 3
-        #20;
-        ui_in = 8'b00000111; // 7
-        uio_in = 8'b00000010; // 2
-        #20;
-        ui_in = 8'b00000000; // 0
-        uio_in = 8'b00000000; // 0
-        #20;
-        ui_in = 8'b00000001; // 1
-        uio_in = 8'b00000001; // 1
-        #20;
-        #10;
-        $stop;
-    end
-    initial begin
-        $monitor("Time=%0d | ui_in=%b, uio_in=%b | uo_out=%b", $time, ui_in, uio_in, uo_out);
-    end
+
+  // Assign values to the wire type inout signals
+  assign ui_in = ui_in_reg;
+  assign uio_in = uio_in_reg;
+
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;  
+  end
+
+  initial begin
+    rst_n = 0;  
+    ui_in_reg = 8'b00000000;
+    uio_in_reg = 8'b00000000;
+    #10;
+    rst_n = 1;  
+    ui_in_reg = 8'b00000011; // 3
+    uio_in_reg = 8'b00000010; // 2
+    #20; 
+    ui_in_reg = 8'b00000001; // 1
+    uio_in_reg = 8'b00000100; // 4
+    #20; 
+    ui_in_reg = 8'b00000101; // 5
+    uio_in_reg = 8'b00000011; // 3
+    #20;
+    ui_in_reg = 8'b00000111; // 7
+    uio_in_reg = 8'b00000010; // 2
+    #20;
+    ui_in_reg = 8'b00000000; // 0
+    uio_in_reg = 8'b00000000; // 0
+    #20;
+    ui_in_reg = 8'b00000001; // 1
+    uio_in_reg = 8'b00000001; // 1
+    #20;
+    #10;
+    $stop;
+  end
+
+  initial begin
+    $monitor("Time=%0d | ui_in=%b, uio_in=%b | uo_out=%b", $time, ui_in, uio_in, uo_out);
+  end
+
 endmodule
